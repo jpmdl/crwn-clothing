241fef25a6067480095129af1d62a8522c32e54d
João Loureiro <jpmdl@hotmail.com>
2020-02-01 09:22:07 +0000

    Stripe payment and favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_HXhFw6YZMwp5QSwgGy3IBH2o0097T1NcJ9";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successfully");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is €${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+      currency="EUR"
+    />
+  );
+};
+
+export default StripeCheckoutButton;
