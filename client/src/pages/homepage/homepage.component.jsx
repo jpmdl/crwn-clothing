2c327b2957599843935f24cc2e25789ac8161567
João Loureiro <jpmdl@hotmail.com>
2020-04-07 17:16:29 +0100

    PWA compliant

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { Profiler } from "react";
 
 import Directory from "../../components/directory/directory.component";
 
 import { HomePageContainer } from "./homepage.styles";
 
+// Profiler lets you analyse the duration that the children took to mount
 const HomePage = () => (
   <HomePageContainer>
-    <Directory />
+    <Profiler
+      id="Directory"
+      onRender={(id, phase, actualDuration) =>
+        console.log({
+          id,
+          phase,
+          actualDuration,
+        })
+      }
+    >
+      <Directory />
+    </Profiler>
   </HomePageContainer>
 );
 
